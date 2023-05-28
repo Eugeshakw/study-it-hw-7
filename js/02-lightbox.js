f735d07cd593239ea2f7f0755426503ad95d1ab5
Eugeshakw <eugeshakw@gmail.com>
2023-05-28 14:25:54 +0300

    upDate

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,47 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const refs = {
+    ul: document.querySelector('.gallery')
+    
+}
+ 
+const gallery = galleryItems.map(({preview, original, description}) => {
+  return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+  <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
+  </img></a></li>`
+
+}).join(' ');
+ 
+refs.ul.innerHTML = gallery;
+
+
+refs.ul.addEventListener('click', openModal);
+
+
+function openModal(evt){
+  if(evt.target.nodeName !== 'IMG'){
+    return;
+  }
+
+  const imgUrl = evt.target.dataset.source;
+  const instance = basicLightbox.create(`
+    <img src="${imgUrl}" width="800" height="600">
+`)
+
+instance.show()
+
+const visible = instance.visible();
+
+document.addEventListener('keydown', closeModal);
+
+function closeModal(evt){
+  if(evt.code === `Escape` && visible){
+    instance.close();
+    document.removeEventListener('keydown', closeModal); 
+  }
+}
+console.log(gallery);
+
+}
